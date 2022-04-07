50ecdf14c82d671a8cdfeb0e497dfa7a46a9ae7b
TriangleSnake <trianglesnake2002@gmail.com>
2022-04-07 12:36:19 +0800

    update eq

--- eq/eq.py
+++ eq/eq.py
@@ -1,6 +1,10 @@
-area='tn' #請輸入警報地區代碼
-region="台南" #請輸入警報地區
-
+try:
+    f=open('setting.txt','r')
+    area=f.readline().split('=')[1]
+    region=f.readline().split('=')[1]
+except:
+    print("設定檔有誤")
+    exit()
 
 import telepot
 import sys
@@ -9,8 +13,6 @@ import datetime
 import time
 bot=telepot.Bot('1908290137:AAFmO6RjCX6XlrRhyIVl-NTKrsZLkCP1QcQ')
 starttime = datetime.datetime.now()
-EQTime=int(starttime.minute)
-CheckTime=starttime.hour*60+starttime.minute
 print(starttime)
 try:
     message="即將發送測試訊息"
@@ -25,9 +27,11 @@ bot.sendMessage('-592407536',message)
 url='https://script.google.com/macros/s/AKfycbws1z7qKfn834vOqQWKi5Z31U7zrwyQo42BsEScE9doYia15SlHIf82aH5v8QdeZQYS/exec?token=ss123'+area
 r=requests.get(url)
 recieve=r.text
+
 if recieve=='-1':
     print("...訊息已由其他電腦發送...")
     exit()
+
 recieve=recieve.split(',')
 recieve.pop()
 for i in recieve:
@@ -55,27 +59,19 @@ requests.get(url)
 import os
 import json
 print("等待地震報告中..")
-for i in range(300):
+for i in range(1):
     print("\r"+str(300-i)+"秒後取得地震報告",end='')
     time.sleep(1)
 url='https://script.google.com/macros/s/AKfycbws1z7qKfn834vOqQWKi5Z31U7zrwyQo42BsEScE9doYia15SlHIf82aH5v8QdeZQYS/exec?token=ss123rep'
 if requests.get(url).text=='-1':
     print("---地震報告已由其他電腦接手---")
     exit()
-while 1:
-    url='https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization=CWB-3F8D970F-C6A9-4090-870F-E8492DD6C97A&format=JSON&areaName=%E8%87%BA%E5%8D%97%E5%B8%82'
-    r=requests.get(url).text
-    r=json.loads(r)
-    EQCheckTime=r['records']['earthquake'][0]['reportContent'][:10]
-    EQCheckDate=EQCheckTime.split("-")[0]
-    EQCheckTime=EQCheckTime.split("-")[1]
-    EQCheckTime=EQCheckTime.split(":")
-    if eval(EQCheckDate[0].split("/")[0])*100+eval(EQCheckDate[0].split("/")[1])==starttime.month*100+starttime.day:
-        if abs(eval(EQCheckTime[0])*60+eval(EQCheckTime[1])-CheckTime)<5:
-            break
-    for i in range(30):
-        print('\r'+str(30-i)+"秒後重新檢測地震報告",end='')
-        time.sleep(1)
+
+url='https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization=CWB-3F8D970F-C6A9-4090-870F-E8492DD6C97A&format=JSON&areaName=%E8%87%BA%E5%8D%97%E5%B8%82'
+r=requests.get(url).text
+r=json.loads(r)
+
+print(r)
 src=r['records']['earthquake'][0]['reportImageURI']
 bot.sendPhoto('-592407536',src)
 for i in recieve:
